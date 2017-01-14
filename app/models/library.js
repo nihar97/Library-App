d45bc24edeb9ea3228e6603b942f94c811374cf7
Adhyyan <adhyyan-n@berkeley.edu>
2017-01-14 19:46:04 +0530

    Filled in the Library Model

--- app/models/library.js
+++ app/models/library.js
@@ -1,1 +1,23 @@
 // this is the model which should have all relevant details of a particular library that has been selected.
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+var bcrypt 		 = require('bcrypt-nodejs');
+
+//user schema
+var LibrarySchema = new Schema({
+	name: { type: String, required: true, index: {unique: true}},
+	location: {type: String, required: true, index: {unique: true}},
+	occupants: {type: Number, required: true 	
+});
+
+LibrarySchema.methods.compareName = function(name) {
+	var library = this;
+	return name.localeCompare(library.name) == 0;
+};
+
+LibrarySchema.methods.occupants = function() {
+	var library = this;
+	return library.occupants;
+};
+
+module.exports = mongoose.model('Library', LibrarySchema);
